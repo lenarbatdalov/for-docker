8592d27a3e080c2efaa78c72c4c78498b351f23c
Lenar Batdalov <developer.lenar@gmail.com>
2020-10-20 13:35:37 +0300

    mysql8 + clientbase.config.php

---
+++ completed/mysql-8-наружу-открыт-для-работы/config.php
@@ -0,0 +1,26 @@
+<?php
+$config["dbtype"] = "mysql";
+$config["dbhost"] = "172.17.0.1";
+$config["dbport"] = "3308";
+$config["dbuser"] = "root";
+$config["dbpass"] = "root";
+$config["dbname"] = "distr";
+$config["table_prefix"] = "f_";
+
+$config["type"] = "WEB";
+$config["version"] = "030001";
+$config["lang"] = "russian";
+
+$config["db_engine"] = "innodb";
+$config["installed"] = 1;
+$config["password_hash_coder"] = "md5";
+$config["help_hash"] = "";
+
+// $config['sql_log'] = 1;  // Логирование бд в папку temp
+
+/*
+AW52-BZ47-RW26
+SE10-JT62-ZS72
+
+folder backup cache files temp templates_c runtime
+*/
