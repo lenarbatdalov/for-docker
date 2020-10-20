eeaa223a8a53d940cd8cfd831ed426dca6fc600e
Lenar Batdalov <developer.lenar@gmail.com>
2020-10-20 14:58:36 +0300

    разные базы + clientbase.config.php

--- completed/разные базы для КБ/config.php
+++ completed/разные базы для КБ/config.php
@@ -1,7 +1,9 @@
 <?php
 $config["dbtype"] = "mysql";
-$config["dbhost"] = "172.17.0.1";
-$config["dbport"] = "3308";
+$config["dbhost"] = "172.17.0.1"; // arch: ip address show | grep docker
+$config["dbport"] = "3357";
+// $config["dbport"] = "3380";
+// $config["dbport"] = "3311";
 $config["dbuser"] = "root";
 $config["dbpass"] = "root";
 $config["dbname"] = "distr";
@@ -16,11 +18,3 @@ $config["installed"] = 1;
 $config["password_hash_coder"] = "md5";
 $config["help_hash"] = "";
 
-// $config['sql_log'] = 1;  // Логирование бд в папку temp
-
-/*
-AW52-BZ47-RW26
-SE10-JT62-ZS72
-
-folder backup cache files temp templates_c runtime
-*/
